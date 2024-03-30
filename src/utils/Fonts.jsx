960b211b1855aa4eb5b0b546c53e981cd535f332
Nicolas Molina <nicolasmolina0920@hotmail.com>
2024-03-30 20:22:06 -0300

    Agregando las fuentes bases para el proyecto

--- src/utils/Fonts.jsx
+++ src/utils/Fonts.jsx
@@ -1,14 +1,27 @@
+import { Container } from 'react-bootstrap';
+
 function Fonts() {
   return (
-    <div>
-      <h1 className="title">Hola Mundo Probando Fuente</h1>
-      <h1 className="subtitle">Hola Mundo Probando Fuente</h1>
-      <h2 className="kanit-light ">Hola Mundo Probando Fuente</h2>
-      <h3 className="kanit-regular">Hola Mundo Probando Fuente</h3>
-      <h4 className="kanit-semibold ">Hola Mundo Probando Fuente</h4>
-      <h5 className="kanit-bold">Hola Mundo Probando Fuente</h5>
-      <h5 className="kanit-extrabold ">Hola Mundo Probando Fuente</h5>
-    </div>
+    <Container className="mt-3">
+      <h1 className="title text-light">Hola Mundo Probando Fuente</h1>
+      <h2 className="title text-light">Hola Mundo Probando Fuente</h2>
+      <h3 className="title text-light">Hola Mundo Probando Fuente</h3>
+      <h4 className="title text-light">Hola Mundo Probando Fuente</h4>
+      <h5 className="title text-light">Hola Mundo Probando Fuente</h5>
+      <h1 className="subtitle text-light mt-5">Hola Mundo Probando Fuente</h1>
+      <h2 className="subtitle text-light">Hola Mundo Probando Fuente</h2>
+      <h2 className="subtitle text-light">Hola Mundo Probando Fuente</h2>
+      <h3 className="subtitle text-light">Hola Mundo Probando Fuente</h3>
+      <h4 className="subtitle text-light">Hola Mundo Probando Fuente</h4>
+      <h5 className="subtitle text-light">Hola Mundo Probando Fuente</h5>
+
+      <p className="paragraph text-light mt-5">
+        Probando este párrafo para ver como queda el texto
+      </p>
+      <p className="small-font text-light">
+        Probando este párrafo para ver como queda el texto
+      </p>
+    </Container>
   );
 }
 export default Fonts;
