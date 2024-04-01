86b78e8292fb025de3ae5ba5522317b072ab2ab5
Macarena <maquibarrios@gmail.com>
2024-04-01 15:38:00 -0300

    Navbar funcional para probar backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import MyNavbar from "./components/MyNavbar";
+import './App.css';
+
+import SignUp from "./components/SignUp";
+import Login from "./components/Login"
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 const App = () => {
   return (
-    <>
-      <h1>Agregamos los componentes en APP</h1>
-    </>
+    <BrowserRouter>
+      <div>
+        <MyNavbar />
+        {/* Aquí puedes agregar el resto de tu contenido de la aplicación */}
+        <div style={{ marginTop: '70px' }}>
+          {/* El estilo marginTop es para dar espacio debajo del navbar */}
+          <h1>Contenido de tu aplicación</h1>
+          {/* Agrega aquí el contenido de tu aplicación */}
+          <Routes>
+            <Route path="/registro" element={<SignUp />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
+
 export default App;
