cc722398c3f703d007399901f9b133a9caf0a384
Ariana Alderete <alderete.ariana.1406@gmail.com>
2024-03-31 22:03:05 -0300

    creacion de ruta y estructura

---
+++ src/pages/planes/Planes.jsx
@@ -0,0 +1,12 @@
+import "../../css/planes.css";
+
+const Planes = () => {
+  return (
+    <>
+      <h1 className="prueba">planess</h1>
+      <h2>en su ruta</h2>
+    </>
+  )
+}
+
+export default Planes
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home/Home';
+import Planes from '../pages/planes/Planes';
 
 const RoutesPages = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/planes" element={<Planes/>} />
       </Routes>
     </>
   );
