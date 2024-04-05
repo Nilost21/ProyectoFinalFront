8dee527696c2a7945f22f97677111cfbddd3b249
Ariana Alderete <alderete.ariana.1406@gmail.com>
2024-04-04 23:54:04 -0300

    formulario de consulta con validaciones basicas

--- src/components/planComponents/FormPlan.jsx
+++ src/components/planComponents/FormPlan.jsx
@@ -2,47 +2,133 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
+import React, { useState } from 'react';
 
 const FormPlan = () => {
+
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [mensaje, setMensaje] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validar nombre y apellido
+    if (nombre.length < 2 || apellido.length < 2) {
+      alert('El nombre y el apellido deben tener al menos 2 caracteres');
+      return;
+    }
+
+    const nombyapRegex = /^[a-zA-ZÀ-ÿ\u00f1\u00d1\s']+$/;
+    if (!nombyapRegex.test(nombre) || !nombyapRegex.test(apellido) ) {
+      alert('Ingrese un nombre o apellido válido');
+      return;
+    }
+
+    // Validar email
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      alert('Ingrese un email válido');
+      return;
+    }
+
+    // Validar teléfono
+    const telefonoRegex = /^[0-9]{10}$/;
+    if (!telefonoRegex.test(telefono)) {
+      alert('El teléfono debe tener 10 dígitos numéricos');
+      return;
+    }
+
+    // Validar mensaje
+    if (mensaje.length > 1500 || mensaje === "") {
+      alert('El mensaje no puede tener más de 1500 caracteres o el campo no debe estar vacio');
+      return;
+    }
+
+    // Si todas las validaciones pasan, puedes enviar el formulario aquí
+    alert('Formulario enviado');
+  };
+
+
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Row className="mb-3">
           <Form.Group as={Col} controlId="formGridNombre">
-            <Form.Label>Nombre</Form.Label>
-            <Form.Control type="text" placeholder="Nombre" />
+            <Form.Control 
+            type="text" 
+            placeholder="First Name" 
+            maxLength={30} 
+            minLength={2}
+            pattern="^[a-zA-ZÀ-ÿ\u00f1\u00d1\s']+$"
+            required 
+            title="Ingresa un nombre válido (solo letras y espacios)" 
+            value={nombre}
+          onChange={(e) => setNombre(e.target.value)}/>
           </Form.Group>
 
           <Form.Group as={Col} controlId="formGridApellido">
-            <Form.Label>Apellido</Form.Label>
-            <Form.Control type="text" placeholder="Apellido" />
+            <Form.Control 
+            type="text" 
+            placeholder="Last Name" 
+            maxLength={30} 
+            minLength={2} 
+            pattern="^[a-zA-ZÀ-ÿ\u00f1\u00d1\s']+$"
+            required
+            title="Ingresa un apellido válido (solo letras y espacios)"
+            value={apellido}
+          onChange={(e) => setApellido(e.target.value)} />
           </Form.Group>
         </Row>
 
         <Row className="mb-3">
           <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+          
+            <Form.Control 
+            type="email" 
+            maxLength={50}
+            title="Ingresa un correo electrónico válido (Ejemplo@ejemplo.com)" 
+            placeholder="Email" 
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" 
+            required 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}/>
           </Form.Group>
 
           <Form.Group as={Col} controlId="formGridTelefono">
-            <Form.Label>Teléfono</Form.Label>
-            <Form.Control type="tel" placeholder="Teléfono" />
+           
+            <Form.Control 
+            type="tel" 
+            maxLength={10}
+            placeholder="Phone" 
+            pattern="[0-9]{10}" 
+            title="Ingresa un número de teléfono válido sin 0 ni 15 (ejemplo: XXX XXXXXXX)"
+            required
+            value={telefono}
+          onChange={(e) => setTelefono(e.target.value)} />
           </Form.Group>
         </Row>
 
         <Row className="mb-3">
-    <Form.Group as={Col} controlId="formGridMensaje">
-      <Form.Label>Mensaje</Form.Label>
-      <Form.Control as="textarea" rows={3} placeholder="Escribe tu mensaje aquí" />
-    </Form.Group>
-  </Row>
+          <Form.Group as={Col} controlId="formGridMensaje">
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="Message"
+              maxLength={1500}
+              
+              value={mensaje}
+          onChange={(e) => setMensaje(e.target.value)}
+            />
+          </Form.Group>
+        </Row>
 
         <Button variant="primary" type="submit">
-          Submit
+          Send Message
         </Button>
       </Form>
-
     </>
   );
 };
