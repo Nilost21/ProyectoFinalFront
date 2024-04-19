14352819b9d8dc97a6a70744d54a6867547ce370
Ariana Alderete <alderete.ariana.1406@gmail.com>
2024-04-19 00:57:33 -0300

    se agregaron portadas a las paginas de planes

--- src/pages/Plans/FullPlan.jsx
+++ src/pages/Plans/FullPlan.jsx
@@ -1,16 +1,65 @@
 import { Container } from 'react-bootstrap';
 import FormPlan from '../../components/planComponents/FormPlan';
+import "../../css/Plans/PlanDetail.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import NavbarComponent from "../../components/NavbarComponent";
+import coverImageFull from "../../assets/img/Plans/cover-image-full-plan.png";
 
 const FullPlan = () => {
   return (
     <>
+
+<Container fluid>
+    <NavbarComponent/>
+    </Container>
+      <header>
+        <div
+          className="cover-section"
+          style={{
+            backgroundImage: `url(${coverImageFull})`,
+            position: "relative",
+          }}
+        >
+          <div className="cover-overlay"></div>
+         
+          <div className="cover-content paragraph" style={{width:'1080px'}}>
+            <h1 className="mb-3 mb-md-5 mt-2">Muscle Only Plan</h1>
+            <p className="me-2 ms-2 small-font mt-5">
+              Achieve your muscle-building goals with our tailored plan.
+              Designed for all fitness levels, it focuses on progressive
+              training and balanced nutrition for optimal muscle growth. Start
+              today and unlock your strength potential.
+            </p>
+            <div className="container-fluid mt-5" style={{width:'310px'}}>
+              <ul>
+                <li>
+                  <i class="bi bi-check-circle-fill me-2"></i>Weightlifting
+                  Exercises
+                </li>
+                <li>
+                  <i class="bi bi-check-circle-fill me-2"></i>Training Routines
+                </li>
+                <li>
+                  <i class="bi bi-check-circle-fill me-2"></i>Premium support
+                  for Diets
+                </li>
+                <li>
+                  <i class="bi bi-check-circle-fill me-2"></i>Includes Personal
+                  Trainer Guidance
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+
     <Container>
-        <header>
-        <h1>plan full</h1>
-        <FormPlan planType="full-plan"/>
-        </header>
-  
-      
+    <p className="paragraph text-light text-center size-title-form mt-4">
+        Want to know more?</p>
+        <p className="paragraph text-light text-center size-subtitle-form">
+          Make your inquiry now!</p>
+        
+        <FormPlan planType="full-plan"/>      
     </Container>
       
     </>
