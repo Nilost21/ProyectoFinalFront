ece12a8495ec58b3bc6af7b072802db440c8314e
Ariana Alderete <alderete.ariana.1406@gmail.com>
2024-04-23 17:49:31 -0300

    se soluciono el responsive

--- src/components/ClassBookingsComponents/ClassBookings.jsx
+++ src/components/ClassBookingsComponents/ClassBookings.jsx
@@ -1,50 +1,63 @@
-{/* Componente que se mostrara en home */}
-{/* Contendra el mapeo de las clases a las cards */}
-import { Container, Row, Col } from 'react-bootstrap';
-import PropTypes from 'prop-types';
-import BookingCard from '../../components/ClassBookingsComponents/BookingCard';
-import { useContext } from 'react';
-import { ClassProvider } from '../../context/ClassContex';
+{
+  /* Componente que se mostrara en home */
+}
+{
+  /* Contendra el mapeo de las clases a las cards */
+}
+import { Container, Row, Col } from "react-bootstrap";
+import PropTypes from "prop-types";
+import BookingCard from "../../components/ClassBookingsComponents/BookingCard";
+import { useContext } from "react";
+import { ClassProvider } from "../../context/ClassContex";
 
 function ClassBookings() {
   const { classes } = useContext(ClassProvider);
 
-   // Función para formatear la fecha y hora al formato deseado
-   const formatDateTime = (dateTimeString) => {
+  // Función para formatear la fecha y hora al formato deseado
+  const formatDateTime = (dateTimeString) => {
     const date = new Date(dateTimeString);
     const formattedDate = date.toLocaleDateString();
-    const formattedTime = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const formattedTime = date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
     return `${formattedDate} ${formattedTime}`;
   };
 
   const isEmpty = () => classes.length === 0;
 
-  
-
   return (
     <>
-        <Container>
-          <Row className="d-flex flex-row justify-content-around mt-3 ">
-            {isEmpty() ? (
-              <h2 className="paragraph">No hay productos</h2>
-            ) : (
-              classes.map((classes) => {
-                const { _id, name, description, teacher, dateAndTime } = classes;
+      <Container>
+        <Row className="d-flex justify-content-center mt-3 ">
+          {isEmpty() ? (
+            <h2 className="paragraph">Oops! No classes available</h2>
+          ) : (
+            classes.map((classes) => {
+              const { _id, name, description, teacher, dateAndTime } = classes;
 
-                return (
-                  <Col xs={8} lg={2} md={6} sm={8} key={_id}>
-                    <BookingCard
-                      name={name}
-                      description={description}
-                      teacher={teacher}
-                      dateAndTime={formatDateTime(dateAndTime)}
-                    />
-                  </Col>
-                );
-              })
-            )}
-          </Row>
-        </Container>
+              return (
+                <Col
+                  xs={12}
+                  sm={12}
+                  md={6}
+                  lg={6}
+                  xl={3}
+                  key={_id}
+                  className="d-flex justify-content-center"
+                >
+                  <BookingCard
+                    name={name}
+                    description={description}
+                    teacher={teacher}
+                    dateAndTime={formatDateTime(dateAndTime)}
+                  />
+                </Col>
+              );
+            })
+          )}
+        </Row>
+      </Container>
     </>
   );
 }
